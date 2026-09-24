efb36199903672431756eee60cd73d43aeaefda1
Ntsakelo <ntsakeloarnold00@gmail.com>
2026-09-24 19:13:13 +0200

    vercel json added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import {Routes,Route} from 'react-router'
+import {Routes,Route, useLocation} from 'react-router'
 import Home from './views/Home'
 import Products from './views/Products'
 import Product from './views/Product'
 import Cart from './views/Cart'
+import { useEffect } from 'react';
+
+
+
+
 
 declare global{
   interface Window {
@@ -11,6 +16,26 @@ declare global{
 }
 
 function App() {
+
+  function ScrollToTopAndTrack() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Check if window.dataLayer exists to avoid crashes
+    window.dataLayer = window.dataLayer || [];
+    
+    window.dataLayer.push({
+      event: 'virtual_page_view',
+      page_path: location.pathname,
+      page_title: document.title
+    });
+  }, [location]);
+
+  return null;
+}
+
+ScrollToTopAndTrack();
+
 
 
   return (
